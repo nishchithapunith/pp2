3e61b1ed9479bc45eecb1736667637f4dc182b2b
Nishchitha L R <nishunishugowda0@gmail.com>
2023-12-29 15:47:56 +0530

    commit successfull

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_pratice_project
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "pp2.txt");
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found: " + filePath);
+
+            }
+        }
+    }
+}
